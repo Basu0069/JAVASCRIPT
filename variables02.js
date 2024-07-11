d556879770a871bb9f3903ce2f7b7c74e21c7a7d
Basu Slathia <basuslathia01@gmail.com>
2024-07-11 16:28:35 +0530

    Update variables02.js

---
+++ variables02.js
@@ -0,0 +1,9 @@
+const accountId =14423  // const ka mtlb hota h aapne jo ek baar memory assign krdi so krdi ab change ni ho sakti 
+// const keyword ko aap change ni kr sakte h okie 
+let accountEmail = "basuslathia@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId =2       you cannot do this twice okie 
+
+console.log(accountId);
