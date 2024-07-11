5d14f6ec3b2f0ad3c0a5a55a876883391ea55b49
Basu Slathia <basuslathia01@gmail.com>
2024-07-12 02:22:41 +0530

    Update variables02.js

--- variables02.js
+++ variables02.js
@@ -4,6 +4,19 @@ let accountEmail = "basuslathia@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
 
-// accountId =2       you cannot do this twice okie 
+// accountId =2       you cannot do this twice okie // not allowed 
 
+
+
+//  now we try to change accountemail nd all the variables 
+accountEmail="dewslathia@gmail.com"
+accountPassword="212122"
+accountCity="Bengaluru"
+let accountState;  // jaise meinee ye term declared ni ki h ab ?
 console.log(accountId);
+
+//  agr ek sath sare print krne h toh table likha kr jaise 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+// NOTE: Prefer not to use var because of issue in block scope and functional scope 
