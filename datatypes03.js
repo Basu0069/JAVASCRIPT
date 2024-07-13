f008a42596b4236920020300be9381c2bf491a8d
Basu Slathia <basuslathia01@gmail.com>
2024-07-13 23:25:21 +0530

    Update datatypes03.js

---
+++ datatypes03.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all JS code as newer version 
+
+// alert(3+3) // we are using nodejs,not browser 
+
+console.log(3
+    +3)       //code readibility should be high 
+
+console.log("Basu")
+
+// remember two sites     ECMA AND MDN reference 
+
+let name = "hitesh"
+let age = 19 
+let isLoggedIn =false 
+
+// datatypes :
+//  number => 2 to power 53 
+//  bigint 
+// string => ""
+// boolean => true/false
+// null=> standalone value 
+// undefined =>
+
+
