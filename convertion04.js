2d06d12a3a9d9f96ad5a503b8deb155bed66b5ce
Basu Slathia <basuslathia01@gmail.com>
2024-07-14 22:01:33 +0530

    Create convertion04.js

---
+++ convertion04.js
@@ -0,0 +1,11 @@
+let score = "33" 
+
+//  isse type pta chl jayega agr koi bhi frontend se data enter kiya toh 
+
+console.log(typeof score);
+console.log(typeof(score));
+
+// AB ham jo score value h jo ki string mei h us datatype ko ham number mei convert krege dekh niche krte h 
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber);
