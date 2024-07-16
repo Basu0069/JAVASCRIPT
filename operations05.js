a353315c0051b9901cb54aca18098ebdbd13c55b
Basu Slathia <basuslathia01@gmail.com>
2024-07-16 15:58:42 +0530

    Update operations05.js

--- operations05.js
+++ operations05.js
@@ -31,5 +31,18 @@ console.log(true); // iska output true aayega
 console.log(+true);  // iska output aayega 1 
 console.log(+"")  // iska output aayega 0 aayega 
 
+//  new concept 
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2+2 
+
+let gameCounter = 100 
+gameCounter++; // tu agr is ++ ko phele bhi pre mei likh dega tb bhi 101 hi aayegi value smja 
+console.log(gameCounter);
+
+// Study Prefix and Postfix on js mdn 
+//  khud kro ye okie
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment 
+
 
 
