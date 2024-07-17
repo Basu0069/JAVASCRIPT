8664f6738e74633d593296e9cc43f1f646c2a6c2
Basu Slathia <basuslathia01@gmail.com>
2024-07-18 01:50:48 +0530

    Update string08.js

--- string08.js
+++ string08.js
@@ -6,9 +6,45 @@ const repoCount = 50
 
 console.log(`Hello my nam is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hiteshhc')
+const gameName = new String('hitesh-hc-com')
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 
 // aap sb prototype ke methdo bhi use kr sakte ho jaise mei krke dikhaat hu 
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+
+console.log(gameName.charAt('2'));
+console.log(gameName.indexOf('t'));
+
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4) // ismei peeche se start kiya h okie smja 
+console.log(anotherString); 
+
+
+
+const newStringOne = "       hitesh         "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+// isse extra spaces hoti h na jo vo remove ho jayegyi..
+// see trim mdn reference okie 
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sundar'));
+// check krega ki iske andar h ye keyword ya ni 
+
+
+
+console.log(gameName.split('-'));
+// split kiya on the basisc of - 
+
+
