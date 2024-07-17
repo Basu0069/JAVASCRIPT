3696b0535c11ace260581e591d33193360839be8
Basu Slathia <basuslathia01@gmail.com>
2024-07-18 00:13:16 +0530

    Create string08.js

---
+++ string08.js
@@ -0,0 +1,14 @@
+const name= "hitesh"
+const repoCount = 50 
+
+// console.log(name + repoCount + " Value");     // ye jo method h ye old h but it will also works but we dont do it okie 
+
+
+console.log(`Hello my nam is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hiteshhc')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+// aap sb prototype ke methdo bhi use kr sakte ho jaise mei krke dikhaat hu 
