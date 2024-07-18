5830b832ed3ba6f109340b9916bf3e58660618ef
Basu Slathia <basuslathia01@gmail.com>
2024-07-19 01:11:26 +0530

    Create nums_and_math09.js

---
+++ nums_and_math09.js
@@ -0,0 +1,31 @@
+const score = 400 
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance)
+
+// in dono mei yhi farak h ki jo neeche vala h ye proper defined h ki ye number h okie 
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+// khud run krke dekh inhe 
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(4));
+
+const hundreds= 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+// isse jo bde number h vo comma lg jayege ismei okie 
+
+
+// You can check on console write Number on it and then type Number.        sb ajaygea 
+
+
+////////////////////MATHS ke baare mei ab/////////
+ 
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,5,6,8));
+// console.log(Math.max(4,5,6,8));
