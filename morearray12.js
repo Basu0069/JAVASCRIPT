1cb33bade9c56cbcb12856423edc68e240a6e316
Basu Slathia <basuslathia01@gmail.com>
2024-07-23 22:46:04 +0530

    Create morearray12.js

---
+++ morearray12.js
@@ -0,0 +1,15 @@
+const marvel_heros = ["thor","Ironman","spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros);
+//  array ko as a ek hi andar le lege apne smja 
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+
+const all_new_heros = 
+
+
