21dc0aa123da80bc83c10cb90be555b4c6695c29
Basu Slathia <basuslathia01@gmail.com>
2024-07-25 00:32:44 +0530

    Create objects13.js

---
+++ objects13.js
@@ -0,0 +1,27 @@
+//  singleton  
+// constructor se ha hamesha singleton bnega 
+// literalls jo delacred krte h toh singleton ni bnta h 
+// object.create   isis method ko bolte h constructor method  baad mei discuss krege ise 
+
+
+
+
+//  object literals 
+//  curly braces hi object h okie 
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "hitesh choudhary",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Saturday"]
+}
+
+console.log(JsUser.email)
+
+
+
+
+
