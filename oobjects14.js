8e675e796c078eb18462e5eff60ba068bac195cb
Basu Slathia <basuslathia01@gmail.com>
2024-07-30 23:23:31 +0530

    Update oobjects14.js

--- oobjects14.js
+++ oobjects14.js
@@ -11,6 +11,31 @@ tinderUser.isLoggedIn= false
 
 
 // console.log(tinderUser);
-4:6 // 
 
-// ihih
+// ab neeche ek aur object declared krege  aur neeche hi object ke andar object declared krege 
+
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+// object combine dekh neeche 
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj2 = {3: "e", 4: "f"}
+
+// const obj3 = { obj1,obj2}
+const obj3 = Object.assign({}, obj1,obj2,obj4)//isse bi combine ho jata h smja // this one prefered most 
+console.log(obj3);
+
+// hihi
+
+
