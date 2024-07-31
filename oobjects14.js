9d0806e4a5c296485d4c3371c6d9c50eba80fe39
Basu Slathia <basuslathia01@gmail.com>
2024-07-31 13:52:19 +0530

    Update oobjects14.js

--- oobjects14.js
+++ oobjects14.js
@@ -30,7 +30,7 @@ const regularUser = {
 // object combine dekh neeche 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
-const obj2 = {3: "e", 4: "f"}
+const obj4 = {3: "e", 4: "f"}
 
 // const obj3 = { obj1,obj2}
 // const obj3 = Object.assign({}, obj1,obj2,obj4)//isse bi combine ho jata h smja //
@@ -42,8 +42,35 @@ const obj3= {...obj1,...obj2} // this one preferred most okie
 
 
 // database se jab values aayegyi toh 
-//  neeche iske andar array of objectss 
+//  neeche iske andar array of objectss aayega 
 
+const users= [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email // iska mtlb h ki aap users ka first part access kr rhe h aur uske andsr email nikal rhe h 
+console.log(tinderUser);
+
+// Aap koi bi object ki value ko aise access kr sakte ho neeche dekh 
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+ 
 
 
 
