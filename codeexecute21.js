236700d41326beff20efb96af095b17b1cb7da17
Basu Slathia <basuslathia01@gmail.com>
2024-08-04 17:23:00 +0530

    Create codeexecute21.js

---
+++ codeexecute21.js
@@ -0,0 +1,31 @@
+Javascript execution context 
+
+
+{}- code h jese 
+phele bnaega GLobal execution context 
+isko reference kr te h "this"
+
+sbse phele 
+* GLobal execution context j 
+* Function execution context 
+* Eval execution context 
+
+
+{} ye do phases mei run hota h 
+1) Memory Creation Phase 
+2) Execution Phase 
+
+
+
+let val1 = 10
+let val2 = 5 
+function addNum(num1,num2){
+    let total = num1 +num2 
+    return total 
+}
+let result1 = addNum(val1,val2)
+let result2= addnum(10,2)
+
+
+
+
