a8e82174ef87c0ffb76dde53639d1ab82c596e83
Basu Slathia <basuslathia01@gmail.com>
2024-08-08 16:33:33 +0530

    Create controlflow22.js

---
+++ controlflow22.js
@@ -0,0 +1,16 @@
+// if 
+
+if(true){
+//  ye code execute hoga 
+}
+if(false){
+
+    // line no. 6 se aage ye execute ni hoga 
+}
+
+// jese hamne neeche liya h ki 
+const isUserLoggedIn = true 
+
+if(isUserLoggedIn){
+
+}
